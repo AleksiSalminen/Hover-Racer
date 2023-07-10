a45d2137335a34abe1c244c17f7791930307efe5
Aleksi Salminen <aleksisalminen1234@gmail.com>
2023-07-10 21:52:21 +0300

    Improved Ladder class

--- engine/bkcore/hexgl/Ladder.js
+++ engine/bkcore/hexgl/Ladder.js
@@ -1,46 +1,55 @@
-/*
-* HexGL
-* @author Thibaut 'BKcore' Despoulain <http://bkcore.com>
-* @license This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License. 
-*          To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/.
-*/
+import Utils from "../Utils.js";
+import Timer from "../Timer.js";
 
-var bkcore = bkcore || {};
-bkcore.hexgl = bkcore.hexgl || {};
 
-bkcore.hexgl.Ladder = {};
-bkcore.hexgl.Ladder.global = {};
+export default class Ladder {
 
-bkcore.hexgl.Ladder.load = function (callback) {
-	var s = encodeURIComponent(window.location.href);
-	bkcore.Utils.request("nothing", false, function (req) {
-		try {
-			bkcore.Ladder.global = JSON.parse(req.responseText);
-			if (callback) callback.call(window);
-		}
-		catch (e) {
-			console.warn('Unable to load ladder. ' + e);
-		}
-	},
+	// ATTRIBUTES
+
+	global;
+
+	// CONSTRUCTORS
+
+	constructor() {
+		this.global = {};
+	}
+
+	// METHODS
+
+	load = function (callback) {
+		let s = encodeURIComponent(window.location.href);
+		Utils.request("nothing", false, function (req) {
+			try {
+				this.global = JSON.parse(req.responseText);
+				if (callback) callback.call(window);
+			}
+			catch (e) {
+				console.warn('Unable to load ladder. ' + e);
+			}
+		},
 		{
 			u: s
 		});
-}
-
-bkcore.hexgl.Ladder.displayLadder = function (id, track, mode, num) {
-	var d = document.getElementById(id);
-	if (d == undefined || bkcore.Ladder.global[track] == undefined || !bkcore.Ladder.global[track][mode] == undefined) {
-		console.warn('Undefined ladder.');
-		return;
 	}
 
-	var l = bkcore.Ladder.global[track][mode];
-	var h = '';
-	var m = Math.min((num == undefined ? 10 : num), l.length - 1);
-	for (var i = 0; i < l.length - 1; i++) {
-		var t = bkcore.Timer.msToTime(l[i]['score']);
-		h += '<span class="ladder-row"><b>' + (i + 1) + '. ' + l[i]['name'] + '</b><i>' + t.m + '\'' + t.s + '\'\'' + t.ms + '</i></span>';
+	displayLadder = function (id, track, mode, num) {
+		let d = document.getElementById(id);
+		if (d == undefined || this.global[track] == undefined || !this.global[track][mode] == undefined) {
+			console.warn('Undefined ladder.');
+			return;
+		}
+	
+		let l = this.global[track][mode];
+		let h = '';
+		let m = Math.min((num == undefined ? 10 : num), l.length - 1);
+		let timer = new Timer();
+		for (let i = 0; i < l.length - 1; i++) {
+			let t = timer.msToTime(l[i]['score']);
+			h += '<span class="ladder-row"><b>' + (i + 1) + '. ' + l[i]['name'] + '</b><i>' + t.m + '\'' + t.s + '\'\'' + t.ms + '</i></span>';
+		}
+	
+		d.innerHTML = h;
 	}
 
-	d.innerHTML = h;
 }
+
