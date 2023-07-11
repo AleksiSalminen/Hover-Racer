55cffb9bf6870794eabf1f828d40e15c074fbc6f
Aleksi Salminen <aleksisalminen1234@gmail.com>
2023-07-11 20:09:41 +0300

    Updated game resource imports, engine moved to src

--- client/src/pages/game/Game.tsx
+++ client/src/pages/game/Game.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Pages, Props } from "../../config/config";
 
-import Engine from "../../../engine/Engine";
+import Engine from "../../engine/Engine";
 
 function Game(props: Props) {
 
@@ -10,12 +10,11 @@ function Game(props: Props) {
     }, []);
 
     const initGame = () => {
-        let defaultControls, url, setting, settings;
+        let defaultControls, url, settings;
         url = Engine.getURLParameter();
         defaultControls = Engine.getDefaultControls();
         settings = [['controlType', ['KEYBOARD', 'TOUCH', 'LEAP MOTION CONTROLLER', 'GAMEPAD'], defaultControls, defaultControls, 'Controls: '], ['quality', ['LOW', 'MID', 'HIGH', 'VERY HIGH'], 3, 3, 'Quality: '], ['hud', ['OFF', 'ON'], 1, 1, 'HUD: '], ['godmode', ['OFF', 'ON'], 0, 1, 'Godmode: ']];
-
-
+        // TODO: Improve settings
         Engine.start(settings[0][3], settings[1][3], settings[2][3], settings[3][3]);
     }
 
