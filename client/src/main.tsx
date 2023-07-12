8c6ad80e4fb499a399f79312a6079c06aa3aa8eb
Aleksi Salminen <aleksisalminen1234@gmail.com>
2023-07-12 16:32:31 +0300

    Turned off React strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+// import React from 'react'; // Remember to re-declare Strict Mode at some point
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  <App />
+);
