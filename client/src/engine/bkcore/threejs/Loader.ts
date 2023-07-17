3029268e256ba3f47e8b5f16f2b3bb4842720cda
Aleksi Salminen <aleksisalminen1234@gmail.com>
2023-07-17 13:20:28 +0300

    Loader to TS

--- client/src/engine/bkcore/threejs/Loader.ts
+++ client/src/engine/bkcore/threejs/Loader.ts
@@ -4,7 +4,7 @@ import ImageData from "../utils/ImageData.js";
 /*!
  * @class bkcore.threejs.Loader
  *
- * Loads multiple recources, get progress, callback friendly.
+ * Loads multiple resources, get progress, callback friendly.
  * Supports textures, texturesCube, geometries, analysers, images.
  * 
  * @author Thibaut 'BKcore' Despoulain <http://bkcore.com>
@@ -15,25 +15,45 @@ export default class LoaderC {
 
 	// ATTRIBUTES
 
-	jsonLoader;
-	errorCallback;
-	loadCallback;
-	progressCallback;
-	types;
-	states;
-	data;
-	progress;
+	jsonLoader: THREE.JSONLoader;
+	errorCallback: (s: string) => void;
+	loadCallback: () => void;
+	progressCallback: (progress: any, type?: string, name?: string) => void;
+	types: {
+		textures: null | any;
+		texturesCube: null | any;
+		geometries: null | any;
+		analysers: null | any;
+		images: null | any;
+		sounds: null | any;
+	};
+	states: { [key: string]: any | {} };
+	data: { [key: string]: any | {} };
+	progress: {
+		total: number;
+		remaining: number;
+		loaded: number;
+		finished: boolean;
+	};
 
 	// CONSTRUCTORS
 
-	constructor(opts) {
-		let self = this;
-
+	constructor(opts: any) {
 		this.jsonLoader = new THREE.JSONLoader();
 
-		this.errorCallback = opts.onError == undefined ? function (s) { console.warn("Error while loading %s.".replace("%s", s)) } : opts.onError;
-		this.loadCallback = opts.onLoad == undefined ? function () { console.log("Loaded.") } : opts.onLoad;
-		this.progressCallback = opts.onProgress == undefined ? function (progress, type, name) { /**/ } : opts.onProgress;
+		this.errorCallback =
+			opts.onError == undefined
+				? function (s: string) {
+					console.warn("Error while loading %s.".replace("%s", s));
+				}
+				: opts.onError;
+		this.loadCallback =
+			opts.onLoad == undefined
+				? function () {
+					console.log("Loaded.");
+				}
+				: opts.onLoad;
+		this.progressCallback = opts.onProgress;
 
 		this.types = {
 			textures: null,
@@ -41,7 +61,7 @@ export default class LoaderC {
 			geometries: null,
 			analysers: null,
 			images: null,
-			sounds: null
+			sounds: null,
 		};
 
 		this.states = {};
@@ -56,7 +76,7 @@ export default class LoaderC {
 			total: 0,
 			remaining: 0,
 			loaded: 0,
-			finished: false
+			finished: false,
 		};
 	}
 
@@ -64,45 +84,44 @@ export default class LoaderC {
 
 	/**
 	 * Load the given list of resources
-	 * @param  {textures, texturesCube, geometries, analysers, images} data 
+	 * @param  {textures, texturesCube, geometries, analysers, images} data
 	 */
-	load(data, audio) {
-		let self = this;
-
+	load(data: { [key: string]: any }, audio?: any) {
 		for (let k in this.types) {
 			if (k in data) {
 				let size = 0;
-				for (let j in data[k])
-					size++;
+				for (let j in data[k]) size++;
 				this.progress.total += size;
 				this.progress.remaining += size;
 			}
 		}
 
-		for (let t in data.textures)
-			this.loadTexture(t, data.textures[t]);
+		for (let t in data.textures) this.loadTexture(t, data.textures[t]);
 
 		for (let c in data.texturesCube)
 			this.loadTextureCube(c, data.texturesCube[c]);
 
-		for (let g in data.geometries)
-			this.loadGeometry(g, data.geometries[g]);
+		for (let g in data.geometries) this.loadGeometry(g, data.geometries[g]);
 
-		for (let a in data.analysers)
-			this.loadAnalyser(a, data.analysers[a]);
+		for (let a in data.analysers) this.loadAnalyser(a, data.analysers[a]);
 
-		for (let i in data.images)
-			this.loadImage(i, data.images[i]);
+		for (let i in data.images) this.loadImage(i, data.images[i]);
 
 		for (let s in data.sounds)
-			this.loadSound(audio, data.sounds[s].src, s, data.sounds[s].loop, data.sounds[s].usePanner);
+			this.loadSound(
+				audio,
+				data.sounds[s].src,
+				s,
+				data.sounds[s].loop,
+				data.sounds[s].usePanner
+			);
 
 		this.progressCallback.call(this, this.progress);
 	}
 
-	updateState(type, name, state) {
+	updateState(type: string, name: string, state: boolean) {
 		if (!(type in this.types)) {
-			console.warn("Unkown loader type.");
+			console.warn("Unknown loader type.");
 			return;
 		}
 
@@ -114,7 +133,6 @@ export default class LoaderC {
 
 		this.states[type][name] = state;
 
-
 		if (this.progress.loaded == this.progress.total) {
 			this.loadCallback.call(this);
 		}
@@ -123,36 +141,36 @@ export default class LoaderC {
 	/**
 	 * Get loaded resource
 	 * @param  string type [textures, texturesCube, geometries, analysers, images]
-	 * @param  string name 
+	 * @param  string name
 	 * @return Mixed
 	 */
-	get(type, name) {
+	get(type: string, name: string) {
 		if (!(type in this.types)) {
-			console.warn("Unkown loader type: " + type);
+			console.warn("Unknown loader type: " + type);
 			return null;
 		}
 		if (!(name in this.data[type])) {
-			console.warn("Unkown file: " + name);
+			console.warn("Unknown file: " + name);
 			return null;
 		}
 
 		return this.data[type][name];
 	}
 
-	loaded(type, name) {
+	loaded(type: string, name: string) {
 		if (!(type in this.types)) {
-			console.warn("Unkown loader type.");
+			console.warn("Unknown loader type.");
 			return null;
 		}
 		if (!(name in this.states[type])) {
-			console.warn("Unkown file.");
+			console.warn("Unknown file.");
 			return null;
 		}
 
 		return this.states[type][name];
 	}
 
-	loadTexture(name, url) {
+	loadTexture(name: string, url: string) {
 		let self = this;
 		let bkcore = { NONE: undefined }; // This is changed, weird
 		this.updateState("textures", name, false);
@@ -168,13 +186,16 @@ export default class LoaderC {
 		);
 	}
 
-	loadTextureCube(name, url) {
+	loadTextureCube(name: string, url: string) {
 		let self = this;
 
 		let urls = [
-			url.replace("%1", "px"), url.replace("%1", "nx"),
-			url.replace("%1", "py"), url.replace("%1", "ny"),
-			url.replace("%1", "pz"), url.replace("%1", "nz")
+			url.replace("%1", "px"),
+			url.replace("%1", "nx"),
+			url.replace("%1", "py"),
+			url.replace("%1", "ny"),
+			url.replace("%1", "pz"),
+			url.replace("%1", "nz"),
 		];
 
 		this.updateState("texturesCube", name, false);
@@ -187,31 +208,25 @@ export default class LoaderC {
 		);
 	}
 
-	loadGeometry(name, url) {
+	loadGeometry(name: string, url: string) {
 		let self = this;
 		this.data.geometries[name] = null;
 		this.updateState("geometries", name, false);
-		this.jsonLoader.load(
-			url,
-			function (a) {
-				self.data.geometries[name] = a;
-				self.updateState("geometries", name, true);
-			}
-		);
+		this.jsonLoader.load(url, function (a: any) {
+			self.data.geometries[name] = a;
+			self.updateState("geometries", name, true);
+		});
 	}
 
-	loadAnalyser(name, url) {
+	loadAnalyser(name: string, url: string) {
 		let self = this;
 		this.updateState("analysers", name, false);
-		this.data.analysers[name] = new ImageData(
-			url,
-			function () {
-				self.updateState("analysers", name, true);
-			}
-		);
+		this.data.analysers[name] = new ImageData(url, function () {
+			self.updateState("analysers", name, true);
+		});
 	}
 
-	loadImage(name, url) {
+	loadImage(name: string, url: string) {
 		let self = this;
 		this.updateState("images", name, false);
 		let e = new Image();
@@ -223,18 +238,13 @@ export default class LoaderC {
 		this.data.images[name] = e;
 	}
 
-	loadSound(audio, src, name, loop) {
+	loadSound(audio: any, src: string, name: string, loop: boolean, usePanner: boolean) {
 		let self = this;
 		this.updateState("sounds", name, false);
 
-		audio.addSound(
-			src,
-			name,
-			loop,
-			function () {
-				self.updateState("sounds", name, true);
-			}
-		);
+		audio.addSound(src, name, loop, function () {
+			self.updateState("sounds", name, true);
+		});
 
 		this.data.sounds[name] = {
 			play: function () {
@@ -243,11 +253,9 @@ export default class LoaderC {
 			stop: function () {
 				audio.stop(name);
 			},
-			volume: function (vol) {
+			volume: function (vol: number) {
 				audio.volume(name, vol);
-			}
+			},
 		};
 	}
-
 }
-
