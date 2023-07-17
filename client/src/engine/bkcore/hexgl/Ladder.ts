26ea5f99953729735aaf5ad9f19c28177b3bc6d3
Aleksi Salminen <aleksisalminen1234@gmail.com>
2023-07-17 10:10:51 +0300

    Ladder class to TS

--- client/src/engine/bkcore/hexgl/Ladder.ts
+++ client/src/engine/bkcore/hexgl/Ladder.ts
@@ -6,7 +6,9 @@ export default class Ladder {
 
 	// ATTRIBUTES
 
-	global;
+	global: {
+		[key: string]: any
+	};
 
 	// CONSTRUCTORS
 
@@ -19,7 +21,7 @@ export default class Ladder {
 	load(callback: Function) {
 		let self = this;
 		let s = encodeURIComponent(window.location.href);
-		Utils.request("nothing", false, function (req) {
+		Utils.request("nothing", false, function (req: any) {
 			try {
 				self.global = JSON.parse(req.responseText);
 				if (callback) callback.call(window);
@@ -33,24 +35,24 @@ export default class Ladder {
 			});
 	}
 
-	displayLadder(id: string, track: string, mode: string, num: number) {
+	displayLadder(id: string, track: string, mode: string, num?: number) {
 		let d = document.getElementById(id);
-		if (d == undefined || this.global[track] == undefined || !this.global[track][mode] == undefined) {
-			console.warn('Undefined ladder.');
-			return;
+		if (d === undefined || d === null || this.global[track] === undefined || !this.global[track][mode] === undefined) {
+		  console.warn('Undefined ladder.');
+		  return;
 		}
-
+	
 		let l = this.global[track][mode];
 		let h = '';
-		let m = Math.min((num == undefined ? 10 : num), l.length - 1);
+		let m = Math.min((num === undefined ? 10 : num), l.length - 1);
 		let timer = new Timer();
 		for (let i = 0; i < l.length - 1; i++) {
-			let t = timer.msToTime(l[i]['score']);
-			h += '<span class="ladder-row"><b>' + (i + 1) + '. ' + l[i]['name'] + '</b><i>' + t.m + '\'' + t.s + '\'\'' + t.ms + '</i></span>';
+		  let t = timer.msToTime(l[i]['score']);
+		  h += '<span class="ladder-row"><b>' + (i + 1) + '. ' + l[i]['name'] + '</b><i>' + t.m + '\'' + t.s + '\'\'' + t.ms + '</i></span>';
 		}
-
+	
 		d.innerHTML = h;
-	}
+	  }
 
 }
 
